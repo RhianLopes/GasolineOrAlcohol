4024a476dd2333833089ab2f650c190f6b69acbb
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-07-22 21:52:09 -0300

    feat: introduce base projact and base title

---
+++ GasolineOrAlcohol/GasolineOrAlcohol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GasolineOrAlcohol
+//
+//  Created by Rhian Lopes da Costa on 22/07/20.
+//  Copyright © 2020 Rhian Lopes da Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
