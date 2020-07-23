61cda29477349fabafd0765ff3bc0ef91ef59b03
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-07-22 22:22:45 -0300

    feat: introduce initial logic

--- GasolineOrAlcohol/GasolineOrAlcohol/ViewController.swift
+++ GasolineOrAlcohol/GasolineOrAlcohol/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gasolinePrice: UITextField!
+    
+    @IBOutlet weak var alcoholPrice: UITextField!
+    
+    @IBOutlet weak var bestGas: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculate(_ sender: Any) {
+        bestGas.text = calculateBestGas()
+    }
+    
+    func validateFields() -> Bool {
+        return !gasolinePrice.text!.isEmpty || !alcoholPrice.text!.isEmpty
+    }
+    
+    func calculateBestGas() -> String {
+        
+        return ""
+    }
 }
 
